6d6cddd47125cb4bc822d2ce078d3c067ffa74ad
Henry Yan <henry.yanbf@gmail.com>
2014-03-14 21:14:11 +0800

    Set root to picklist.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Certus::Application.routes.draw do
   # You can have the root of your site routed with "root"
-  root 'deliveries#index'
+  root 'deliveries#picklist'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
