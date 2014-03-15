029e662a7eae5dfb426265587becd58d91b51995
Frank <fudf@shinetechchina.com>
2014-03-15 16:18:21 +0800

    add validate process for delivery

--- app/models/delivery.rb
+++ app/models/delivery.rb
@@ -1,3 +1,17 @@
 class Delivery < ActiveRecord::Base
   has_many :delivery_items
+
+  validates_presence_of :shipping_address
+  validates_associated :delivery_items
+  validate :order_to_delivery_convert
+
+
+  def order_to_delivery_convert
+    if order_grand_total != payment_amount
+      self.errors.add(:order_grand_total,"order_grand_total doesn't match payment_amount")
+    end
+    if order_sku_count != delivery_items.size()
+      self.errors.add(:order_sku_count,"sku_count doesn't match number of order_items")
+    end
+  end
 end
--- app/models/delivery_item.rb
+++ app/models/delivery_item.rb
@@ -1,3 +1,12 @@
 class DeliveryItem < ActiveRecord::Base
   belongs_to :delivery
+
+  validate :order_to_delivery_convert
+
+
+  def order_to_delivery_convert
+    if client_sku != store_sku
+      self.errors.add(:client_sku,"order_grand_total doesn't match payment_amount")
+    end
+  end
 end
