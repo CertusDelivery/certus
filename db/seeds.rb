4b083a20eb2c846c35c95ee197fbbb67a41d6792
Henry Yan <henry.yanbf@gmail.com>
2014-03-17 20:42:06 +0800

    Updating db seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,10 +15,14 @@ deliveries << Delivery.create(order_status: 'UNPICKED')
 deliveries << Delivery.create(order_status: 'UNPICKED')
 
 deliveries.each do |delivery|
-    delivery.delivery_items << [DeliveryItem.create(picked_status: '1/0/0/0', product_name: 'Special K Cereal - Strawberry',
-                                    shipping_weight: '18.0',shipping_weight_unit: 'liter', location: '12N-45-4', picked_quantity: 1, picker_bin_number: '12400', store_sku: '6143854'),
-                                DeliveryItem.create(picked_status: '2/0/0/0', product_name: 'Purell Hand Sanitizer',
-                                    shipping_weight: '12.6',shipping_weight_unit: 'oz', location: '7N-30-3', picked_quantity: 3, picker_bin_number: '12345', store_sku: '073852400908'),
-                              DeliveryItem.create(picked_status: '1/1/0/0', product_name: 'Diet Coke',
-                                    shipping_weight: '12.6', shipping_weight_unit: 'oz', location: '7N-35-3', picked_quantity: 3, picker_bin_number: '12345', store_sku: '073852400908')]
+    delivery.delivery_items << [
+      DeliveryItem.create(picked_status: '1/0/0/0', product_name: 'Special K Cereal - Strawberry', quantity: 1,
+        shipping_weight: '18.0',shipping_weight_unit: 'liter', location: '12N-45-4', picked_quantity: 0,
+        picker_bin_number: '12400', store_sku: '6143854'),
+      DeliveryItem.create(picked_status: '2/0/0/0', product_name: 'Purell Hand Sanitizer', quantity: 2,
+        shipping_weight: '12.6',shipping_weight_unit: 'oz', location: '7N-30-3', picked_quantity: 0,
+        picker_bin_number: '12345', store_sku: '073852400908'),
+      DeliveryItem.create(picked_status: '1/1/0/0', product_name: 'Diet Coke', shipping_weight: '12.6', quantity: 1,
+        shipping_weight_unit: 'oz', location: '7N-35-3', picked_quantity: 1, picker_bin_number: '12345',
+        store_sku: '073852400908')]
 end
