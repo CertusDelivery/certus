19aac38352e0bcbc3a8ad618c77c8a9fe6e9b98d
Henry Yan <henry.yanbf@gmail.com>
2014-03-18 11:10:19 +0800

    Updating db seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,22 +7,23 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 Delivery.delete_all
 deliveries = []
-deliveries << Delivery.create(order_status: 'PICKING')
-deliveries << Delivery.create(order_status: 'PICKING')
-deliveries << Delivery.create(order_status: 'PICKING')
-deliveries << Delivery.create(order_status: 'UNPICKED')
-deliveries << Delivery.create(order_status: 'UNPICKED')
-deliveries << Delivery.create(order_status: 'UNPICKED')
+deliveries << Delivery.new(order_sku_count: 3, order_status: 'PICKING', shipping_address: "test shipping_address")
+deliveries << Delivery.new(order_sku_count: 3, order_status: 'PICKING', shipping_address: "test shipping_address")
+deliveries << Delivery.new(order_sku_count: 3, order_status: 'PICKING', shipping_address: "test shipping_address")
+deliveries << Delivery.new(order_sku_count: 3, order_status: 'UNPICKED', shipping_address: "test shipping_address")
+deliveries << Delivery.new(order_sku_count: 3, order_status: 'UNPICKED', shipping_address: "test shipping_address")
+deliveries << Delivery.new(order_sku_count: 3, order_status: 'UNPICKED', shipping_address: "test shipping_address")
 
 deliveries.each do |delivery|
     delivery.delivery_items << [
       DeliveryItem.create(picked_status: '1/0/0/0', product_name: 'Special K Cereal - Strawberry', quantity: 1,
         shipping_weight: '18.0',shipping_weight_unit: 'liter', location: '12N-45-4', picked_quantity: 0,
-        picker_bin_number: '12400', store_sku: '6143854'),
+        picker_bin_number: '12400', store_sku: '6143854', client_sku: '6143854'),
       DeliveryItem.create(picked_status: '2/0/0/0', product_name: 'Purell Hand Sanitizer', quantity: 2,
         shipping_weight: '12.6',shipping_weight_unit: 'oz', location: '7N-30-3', picked_quantity: 0,
-        picker_bin_number: '12345', store_sku: '073852400908'),
+        picker_bin_number: '12345', store_sku: '073852400908', client_sku: '073852400908'),
       DeliveryItem.create(picked_status: '1/1/0/0', product_name: 'Diet Coke', shipping_weight: '12.6', quantity: 1,
         shipping_weight_unit: 'oz', location: '7N-35-3', picked_quantity: 1, picker_bin_number: '12345',
-        store_sku: '073852400908')]
+        store_sku: '073852400908', client_sku: '073852400908')]
+   delivery.save
 end
