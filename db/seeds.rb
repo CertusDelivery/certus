bebac2ad48cfcbb1ad2f218b3f64eeeb9610698d
Henry Yan <henry.yanbf@gmail.com>
2014-03-21 00:26:42 +0800

    Using factory_girl. Updating seeds.rb.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,48 +1,32 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'factory_girl'
+require 'ffaker'
+Dir["#{Rails.root}/spec/factories/**/*.rb"].each { |f| require f }
+
 Delivery.delete_all
+DeliveryItem.delete_all
 deliveries = []
 
-def create_delivery(item)
-	delivery = Delivery.new( customer_name: Faker::Name.name,
-								 shipping_address: Faker::Address.street_address(true),
-								 customer_email: 'test@test.com',
-								 order_id: item,
-	               order_piece_count:3,
-								 payment_id:rand(9999),
-								 payment_card_token: rand(999999999).to_s,
-	               payment_amount: 1000,
-								 order_status: 'IN_FULFILLMENT',
-								 picked_status: 'UNPICKED',
-								 order_grand_total: 500,
-								 payment_amount: 500,
-								 order_sku_count: 3,
-								 order_total_price: 500,
-								 placed_at: Date.today
-	               )
-
-	delivery.delivery_items = [
-			DeliveryItem.create(picked_status: '1/0/0/0', product_name: 'Special K Cereal - Strawberry', quantity: 1,
-													shipping_weight: '18.0',shipping_weight_unit: 'liter', location: '12N-45-4', picked_quantity: 0,
-													picker_bin_number: '12400', store_sku: '6143854', client_sku: '6143854', price: 200),
-			DeliveryItem.create(picked_status: '2/0/0/0', product_name: 'Purell Hand Sanitizer', quantity: 2,
-													shipping_weight: '12.6',shipping_weight_unit: 'oz', location: '7N-30-3', picked_quantity: 1,
-													picker_bin_number: '12345', store_sku: '073852400908', client_sku: '073852400908', price: 200),
-			DeliveryItem.create(picked_status: '1/1/0/0', product_name: 'Diet Coke', shipping_weight: '12.6', quantity: 1,
-													shipping_weight_unit: 'oz', location: '7N-35-3', picked_quantity: 1, picker_bin_number: '12345',
-													store_sku: '073852400908', client_sku: '073852400908', price: 100)]
-	delivery
-	delivery.save!
-
+def create_delivery(picked_status = :unpicked)
+  item_count = rand(10) + 1
+  delivery = FactoryGirl.build :delivery, picked_status, order_sku_count: item_count
+  total_price = 0
+  item_count.times do
+    delivery_item = FactoryGirl.build(:delivery_item)
+    delivery.delivery_items << delivery_item
+    total_price += delivery_item.price
+  end
+  delivery.payment_amount = delivery.order_grand_total = delivery.order_total_price = total_price
+  delivery.save!
 end
 
 100.times { |item|
-	puts "Create delivery #{item+1}"
-	create_delivery item
+  puts "Create unpicked delivery #{item+1}"
+  create_delivery
 }
+
+2.times { |item|
+  puts "Create picking delivery #{item+1}"
+  create_delivery :picking
+}
+
 
---
+++ spec/factories/delivery_item_factory.rb
@@ -0,0 +1,18 @@
+FactoryGirl.define do
+  factory :delivery_item do
+    picked_quantity 0
+    store_sku { Faker.numerify('###############') }
+    client_sku { store_sku }
+    location { "#{Faker.numerify('##')}#{%w{N S E W}.sample}-#{Faker.numerify('##')}-#{Faker.numerify('#')}"}
+    shipping_weight { Faker.numerify('##.##').to_f }
+    picker_bin_number { Faker.numerify('#######') }
+    payment_adjustment 0
+    product_name { Faker::Product.product_name }
+    quantity { Faker.numerify('#') }
+    price { Faker.numerify('##.##') }
+    tax { Faker.numerify('#.##') }
+    shipping_weight_unit { %w{oz kg g liter}.sample }
+    out_of_stock_quantity 0
+    scanned_quantity 0
+  end
+end
