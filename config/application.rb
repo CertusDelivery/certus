ac3d5df5812044729cdfb264ca41c07f7e435c89
Henry Yan <henry.yanbf@gmail.com>
2014-05-04 21:47:07 +0800

    Adding additional assets to precompile

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,7 @@ Bundler.require(:default, Rails.env)
 module Certus
   class Application < Rails::Application
 
-    config.assets.precompile += ['customer.css', 'login.css', 'admin.css']
+    config.assets.precompile += ['customer.css', 'login.css', 'admin.css', 'bootstrap.min.css']
     config.assets.precompile += ['customer.js', 'admin.js']
 
     # Settings in config/environments/* take precedence over those specified here.
